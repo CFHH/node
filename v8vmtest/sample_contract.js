36d5deb3cd4d0104dd3e0f52532b6d614ab8a048
CFHH <13436361@qq.com>
2019-07-09 14:32:06 +0800

    测试下BigInt和Long

--- v8vmtest/sample_contract.js
+++ v8vmtest/sample_contract.js
@@ -18,6 +18,22 @@ function Process(param)
     var l2 = Long.fromString("1234567898642");
     var l3 = l1.add(l2)
     log(l1.toString() + " + " + l2.toString() + " = " + l3.toString());
+
+    var b1 = BigInt(1234567897531);
+    var b2 = BigInt(1234567898642);
+    var b3 = b1 + b2;
+    log(b1.toString() + " + " + b2.toString() + " = " + b3.toString());
+
+    var obj = {};
+    obj.name = typeof b1;
+    obj.vint = 3;
+    //obj.vbigint = b3;
+    obj.arr = new Array();
+    var str = JSON.stringify(obj);
+    log("obj = " + str);
+    obj.arr[0] = "zzw";
+    str = JSON.stringify(obj);
+    log("obj = " + str);
     return 0;
 }
 
